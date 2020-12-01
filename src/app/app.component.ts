4b44241bebc3c5d2b8675bcf98106aa67e6985ce
kaminikuhad <kamini.kuhad09@gmail.com>
2020-12-01 13:20:29 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, VERSION } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+  constructor(private http: HttpClient) {}
+  abc: any = [];
+  abc1: any = [];
+  abc3: any;
+  ngOnInit() {
+    this.abc = this.http
+      .get("https://jsonplaceholder.typicode.com/users")
+      .subscribe((res: any) => {
+        console.log(res);
+        this.abc1 = res;
+        console.log(this.abc1);
+      });
 
+    //this.abc = this.ab
+  }
+}
---
+++ src/app/app.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, Injectable, OnInit, VERSION } from "@angular/core";
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ServiceClass {
+
+}
